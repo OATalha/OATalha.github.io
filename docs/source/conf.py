5ce762c409c1cd7ba885ef472b7a7d3c3cf28508
Talha Ahmed <talha.ahmed@moonbug.com>
2022-10-06 15:56:22 +0800

    add docs

---
+++ docs/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "OATalha's Doc Test"
+copyright = '2022, Talha Ahmed'
+author = 'Talha Ahmed'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+extensions = [
+        'sphinx.ext.intersphinx',
+        'sphinx.ext.extlinks',
+        'sphinx.ext.autodoc',
+        'sphinx.ext.autosummary'
+]
+exclude_patterns = []
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'arithmetics': (
+        'https://oatalha.github.io/mymath.arithmetics', None),
+    'trigonometry': (
+        'https://oatalha.github.io/mymath.trigonometry', None),
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,27 @@
+.. My Math documentation master file, created by
+   sphinx-quickstart on Wed Sep 28 19:58:56 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to My Math's documentation!
+===================================
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+   Introduction <intro>
+   Background <background>
+   Repositories <repos>
+   API Documentation <apidocs>
+   Conclusion <conclusion>
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
