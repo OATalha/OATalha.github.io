c1b37333c172bca057156a72d7082c3138c7d30e
Talha Ahmed <talha.ahmed@moonbug.com>
2022-10-12 15:09:05 +0800

    trying stuff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,30 +14,30 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+import os
+import sys
+
+sys.path.append(os.path.abspath('./'))
+
 extensions = []
 
 templates_path = ['_templates']
 extensions = [
-        'sphinx.ext.intersphinx',
-        'sphinx.ext.extlinks',
-        'sphinx.ext.autodoc',
-        'sphinx.ext.autosummary',
-        'sphinx_affiliates',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.extlinks',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx_affiliates',
 ]
 exclude_patterns = []
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
-    'arithmetics': (
-        'https://oatalha.github.io/mymath.arithmetics', None),
-    'trigonometry': (
-        'https://oatalha.github.io/mymath.trigonometry', None),
+    'arithmetics': ('https://oatalha.github.io/mymath.arithmetics', None),
+    'trigonometry': ('https://oatalha.github.io/mymath.trigonometry', None),
 }
 
-affiliate_options = {
-        'canonical_url': "https://oatalha.github.io/"
-        }
-
+affiliate_options = {'canonical_url': "https://oatalha.github.io/"}
 
 sphinx_affiliates = [
     'https://oatalha.github.io/mymath.arithmetics/affiliate_searchindex.js',
@@ -47,7 +47,7 @@ sphinx_affiliates = [
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
 html_logo = './_static/One-Animation-Logo-Small.png'
 html_favicon = './_static/favicon.ico'
